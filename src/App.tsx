95781f97c34d25667d274a5b1488dca5c4b174d1
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-06-19 22:05:59 -0400

    play as dealer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './422px-Blackjack.svg.png';
 import './App.css';
 import { Dealer } from './util/Blackjack';
@@ -7,10 +7,24 @@ import Card from './components/Card';
 
 function App() {
 
+  enum Person {
+    User = 'User',
+    Dealer = 'Dealer',
+    Nobody = 'Nobody'
+  }
+
   const dealer = new Dealer();
+
 
   const [cards, setCards] = useState<IFullCard[]>([]);
   const [dealerCards, setDealerCards] = useState<IFullCard[]>([]);
+
+  const [isBlackjack, setIsBlackJack] = useState(false);
+
+
+  const [hasDealt, setHasDealt] = useState(false);
+  const [hasStayed, setHasStayed] = useState(false);
+
 
 
   const [userValue, setUserValue] = useState(0);
@@ -20,10 +34,35 @@ function App() {
   // we need to start by dealing with the default deck; then each subsequent deal should draw from the curent deck;
 
 
+  const [didPlayerLose, setDidPlayerLose] = useState(false);
+  const [didDealerWin, setDidDealerWin] = useState(false);
+  const [didPLayerWin, setDidPlayerWin] = useState(false);
+
+
+  useEffect(() => {
+
+    if (userValue === 21) {
+      setIsBlackJack(true);
+    }
+
+    if (userValue > 21) {
+      setWinner(Person.Dealer)
+    }
+  }, [userValue]);
+
+  useEffect(() => {
+
+    if (dealerValue > 21) {
+      setWinner(Person.User);
+    }
+  }, [dealerValue])
+
+
+
 
   function dealCards() {
     const { cards, updatedDeck } = dealer.dealCards(currDeck, 2);
-    console.log(cards);
+    // console.log(cards);
     setCards(cards);
 
 
@@ -41,11 +80,10 @@ function App() {
     let dealerValue = getValueFromCards(dealer_cards);
     setDealerValue(dealerValue);
 
+    setHasDealt(true);
+
   }
 
-  function incrementValue(newValue: number) {
-    setUserValue(userValue => userValue + newValue);
-  }
 
 
   function getValueFromCards(cards: IFullCard[]) {
@@ -65,10 +103,111 @@ function App() {
     const mergedCards = [...cards, ...hitCard]
     setCards(mergedCards);
     setCurrDeck(updatedDeck);
+
+    // update user value
+    let val = getValueFromCards(mergedCards);
+    setUserValue(val);
   }
 
   function stay() {
+    setHasStayed(true);
 
+    // go on to let the dealer play
+    playAsDealer();
+  }
+
+
+  function playAsDealer() {
+    console.log('playAsDealer()')
+    // if user has chosen to "stay", dealer needs to play until they beat the user or go over 21
+
+
+    // check if dealer has greater value than user
+    // if they have, dealer wins
+
+    const isDealerValGreater = dealerValue > userValue;
+    if (isDealerValGreater && dealerValue <= 21) {
+      setWinner(Person.Dealer);
+    } else if (dealerValue > 21) {
+      console.log('dealerValue > 21')
+      setWinner(Person.User) // ✍ WE NEED TO HANDLE THIS EXCEPTION WHERE NOBODY WINS
+    } else {
+      // continue hitting until dealer value is greater than player, or dealer loses
+
+      const handle = setInterval(() => {
+
+        if (dealerValue === 21) {
+
+          if (userValue !== 21) {
+            setWinner(Person.Dealer);
+          } else {
+            clearRound();
+          }
+          clearInterval(handle);
+
+        } else if (dealerValue > 21) {
+          console.log('dealerValue > 21')
+          setWinner(Person.User) // ✍ WE NEED TO HANDLE THIS EXCEPTION WHERE NOBODY WINS
+          clearInterval(handle);
+          
+        } else if (dealerValue > userValue && dealerValue <= 21) {
+          console.log('dealerValue > userValue && dealerValue <=21')
+
+          // dealer wins
+          setWinner(Person.Dealer);
+          clearInterval(handle);
+        } else if (dealerValue < userValue) {
+          console.log('dealerValue < userValue')
+
+          // keep hitting
+          let { cards: hitCard, updatedDeck } = dealer.dealCards(currDeck, 1);
+          let mergedCards = [...dealerCards, ...hitCard]
+
+          setDealerCards(mergedCards);
+          setCurrDeck(updatedDeck);
+
+          let dealerValue = getValueFromCards(mergedCards);
+          setDealerValue(dealerValue);
+
+        } else {
+          clearInterval(handle);
+        }
+      }, 2000)
+
+    }
+  }
+
+  function setWinner(person: Person) {
+    if (person === Person.User) {
+      setDidDealerWin(false);
+      setDidPlayerWin(true);
+    } else if (person === Person.Dealer) {
+      setDidDealerWin(true);
+      setDidPlayerWin(false);
+      setDidPlayerLose(true);
+    } else if (person === Person.Nobody) {
+      setDidDealerWin(false);
+      setDidPlayerWin(false);
+      setDidPlayerLose(false);
+    }
+
+    clearRound();
+  }
+
+
+  function clearRound() {
+    setTimeout(() => {
+      setDidDealerWin(false);
+      setDidPlayerLose(false);
+      setDidPlayerWin(false);
+      setUserValue(0);
+      setDealerValue(0);
+      setCards([]);
+      setDealerCards([]);
+      setHasDealt(false);
+
+      setHasStayed(false);
+    }, 2000)
   }
 
   return (
@@ -81,22 +220,33 @@ function App() {
         <h5>currDeck length: {currDeck.length}</h5>
 
 
+        {
+          didPLayerWin && <div className="bg-success">You win!</div>
+        }
+
+
         <div className="row">
           <div className="col">
-            <button className="btn btn-secondary" onClick={dealCards}>Deal</button>
+            {!hasDealt &&
+              <button className="btn btn-secondary" onClick={dealCards}>Deal</button>}
           </div>
           <div className="col">
-            <button className="btn btn-secondary" onClick={() => hit()}>Hit</button>
+            {hasDealt && !hasStayed &&
+              <button className="btn btn-secondary" onClick={() => hit()}>Hit</button>}
           </div>
           <div className="col">
-            <button className="btn btn-secondary" onClick={() => stay()}>stay</button>
+            {hasDealt && !hasStayed &&
+              <button className="btn btn-secondary" onClick={() => stay()}>stay</button>}
           </div>
         </div>
 
 
 
+
         <div className="row">
           <div className="col-lg-6">
+            {didPlayerLose && <div className="bg-danger" style={{ visibility: 'hidden' }}>A</div>}
+            {isBlackjack && <div className="bg-success">Blackjack!</div>}
             <h2>Your cards</h2>
             <div>{userValue.toString()}</div>
             <div>
@@ -108,6 +258,9 @@ function App() {
             </div>
           </div>
           <div className="col-lg-6">
+            {
+              didDealerWin && <div className="bg-warning">Dealer wins!</div>
+            }
             <h2>Dealer's cards</h2>
             <div>{dealerValue.toString()}</div>
             {dealerCards.map(card => (
